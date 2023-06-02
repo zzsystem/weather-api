61a0cae2f4e5882927ea987cb1f686e4141e8a22
Madari Zoltán <zoltan.madari@zzsys.hu>
2023-06-02 18:45:49 +0200

    fix: request

--- app/Http/Requests/WeatherInformationRequest.php
+++ app/Http/Requests/WeatherInformationRequest.php
@@ -13,7 +13,7 @@ class WeatherInformationRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,7 @@ class WeatherInformationRequest extends FormRequest
     public function rules()
     {
         return [
-            'city' => 'required|string'
+            'query' => 'required|string'
         ];
     }
 }
